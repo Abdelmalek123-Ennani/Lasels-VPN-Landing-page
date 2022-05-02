39d242f96d97307bea17b717d25007016f11e131
Abdelmalek_Ennani <67714218+Abdelmalek123-Ennani@users.noreply.github.com>
2022-05-02 23:36:43 +0000

    upload the project

---
+++ js/script.js
@@ -0,0 +1,92 @@
+
+
+// variables
+
+const btnLeft = document.querySelector(".left-right .left");
+const btnRight = document.querySelector(".left-right .right");
+const tesTimonialContainer =  document.querySelector(".testimonial");
+const cardWidth = document.querySelector(".testimonial .card").clientWidth;
+
+// navbat
+const navBtn = document.querySelector(".navbar .open-close i");
+const navElements = document.querySelector(".navbar nav");
+
+
+const points = document.querySelectorAll(".dotes .dote");
+let pointIndex = 0;
+
+btnLeft.addEventListener('click' , () => {
+   
+
+    if ( pointIndex != 0 ) pointIndex--; 
+
+    points.forEach((e)  => {
+        if ( e.classList.contains("active"))  e.classList.remove("active");
+    }) 
+
+    points[pointIndex].classList.add("active")
+
+    tesTimonialContainer.scrollTo({
+        behavior : "smooth",
+        left : tesTimonialContainer.scrollLeft > 0 ? tesTimonialContainer.scrollLeft - parseInt(cardWidth) : 0
+    })
+
+
+})
+
+btnRight.addEventListener('click' , () => {
+    tesTimonialContainer.scrollTo({
+        behavior : "smooth",
+        left : tesTimonialContainer.scrollLeft + parseInt(cardWidth)
+    });
+    
+    if ( pointIndex == 3 ) pointIndex = 3;
+    else pointIndex++;
+
+    points.forEach((e)  => {
+        if ( e.classList.contains("active"))  e.classList.remove("active");
+    }) 
+
+    points[pointIndex].classList.add("active")
+
+})
+
+
+
+points.forEach((btn , idx) => {
+    btn.addEventListener('click' , () => {
+
+        
+    tesTimonialContainer.scrollTo({
+        behavior : "smooth",
+        left :  (cardWidth * idx)
+    });
+    
+        pointIndex = idx;
+        points.forEach((e)  => {
+            if ( e.classList.contains("active"))  e.classList.remove("active");
+        }) 
+        points[pointIndex].classList.add("active")
+    });
+
+
+})
+
+
+// toggle navbar 
+
+navBtn.addEventListener('click' , (e) => {
+
+      if ( navElements.style.height == "0px" ) {
+          console.log(navElements.style.height)
+            navElements.style.height = "auto"
+            navElements.style.border = "solid 1px #333"
+            navElements.querySelector("ul").style.display = "block"
+      } else {
+        console.log(navElements.style.height)
+        navElements.style.height = "0px"
+        navElements.style.border = "none"
+        navElements.querySelector("ul").style.display = "none"
+      }
+
+})
